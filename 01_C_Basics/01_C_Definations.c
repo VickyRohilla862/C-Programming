07eac67c17b50690db1c1ec7f1e2b7820c667986
VickyRohilla862 <145284256+VickyRohilla862@users.noreply.github.com>
2025-09-04 15:08:21 +0000

    deleted:    01_C_Basics/01_C_Definations 	new file:   01_C_Basics/01_C_Definations.c 	new file:   01_C_Basics/02_C_Structure.c

---
+++ 01_C_Basics/01_C_Definations.c
@@ -0,0 +1,3 @@
+// C is created by Dennis Richie. He created it in AT&T's Bell Laboratories of USA in 1972.
+// It is the fastest programming language so it is used to make operating systems like Windows and Mac.
+//Boilorplate Template: A template of a particular code that is used in every program is known as boilorplate template
---
+++ 01_C_Basics/02_C_Structure.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //preprocessor statement that imports the basic input and output functions to the file
+
+int main(){ //main function that runs on the time of execution
+    printf("Hello World!"); //main code
+    return 0; //indicated the end of the program
+}
