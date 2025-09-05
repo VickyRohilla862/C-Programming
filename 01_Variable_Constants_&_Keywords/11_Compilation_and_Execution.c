f6b8d43478f301257b19c8b011436427fa05f706
VickyRohilla862 <145284256+VickyRohilla862@users.noreply.github.com>
2025-09-05 15:09:13 +0000

    new file:   01_Variable_Constants_&_Keywords/11_Compilation_and_Execution.c 	new file:   01_Variable_Constants_&_Keywords/12_Library_Functions.c 	new file:   01_Variable_Constants_&_Keywords/c-compiling.png

---
+++ 01_Variable_Constants_&_Keywords/11_Compilation_and_Execution.c
@@ -0,0 +1,5 @@
+// Check the image file named "c-compiling.png" fro better understanding.
+
+// A compiler is a computer program which converts a C program into machine language so that it can be easily understood by the computer.
+
+// A C program is written in plain text. This plain text is combination of instructons in a particular sequence. The compiler perform some basic checks and finally converts the program into an executable.
---
+++ 01_Variable_Constants_&_Keywords/12_Library_Functions.c
@@ -0,0 +1,19 @@
+
+
+// C language has a lot of valuable library functions which is used to carry out certain tasks.
+// For instance, printf() function is used to print values on screen.
+
+//example:
+
+#include <stdio.h>
+int main(){
+    int i = 10;
+    printf("This id %d\n", i);
+    // %d for integers
+    // %f fro floating point numbers
+    // %c for characters
+    return 0;
+}
+
+//output:
+// This is 10
