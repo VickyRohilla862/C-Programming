f4a1ccb692b4e0189a9865fe389d37e03c39ea91
VickyRohilla862 <145284256+VickyRohilla862@users.noreply.github.com>
2025-09-05 07:56:50 +0000

    new file:   01_Variable_Constants_&_Keywords/10_Comments.c

---
+++ 01_Variable_Constants_&_Keywords/10_Comments.c
@@ -0,0 +1,17 @@
+// Comments are used to clerify something about the program in plain language.
+// It is a way for us to add notes to our programs.
+// There are 2 types of comments in C: 
+/*single- line comment: single line comments starts with two forward slashes(//).
+Any information lying after the slashes lying on the same line would be ignored(will not the executed)
+example: 
+    // This is a single line comment
+*/
+
+/*multi line comment: A multi line comment starts with /* and ends with */
+/*Any information between these symbols will be ignored by the compiler*/
+//example:
+    /*
+        This is a multi line comment.
+    */
+
+//Note: Comments is not executed and ignored.
