b46498ee0741e6f860522998ccf64944caeee77e
VickyRohilla862 <145284256+VickyRohilla862@users.noreply.github.com>
2025-09-05 07:00:02 +0000

    modified:   01_Variable_Constants_&_Keywords/04_Rules for Naming Variables.c

--- 01_Variable_Constants_&_Keywords/04_Rules for Naming Variables.c
+++ 01_Variable_Constants_&_Keywords/04_Rules for Naming Variables.c
@@ -4,9 +4,22 @@
 
 // Special symbols and blank spaces are not allowed.
 
-// Keywords or reserved words are not allowed.
+// Keywords or reserved words are not allowed. 
 
 /* Both uppercase and lowercase letters are allowed and are considered to be distinguishable
 example: The variable SUM is not same as sum or Sum.
 */
 
+/*A variable name may be as long as you wish, but the first 8 characters of a variable are recognized by C compilers.
+The variable name "accountsbranch" and "accountoffice" would be regarded as identical variables in C.
+example: 
+    int a; and int A; are not same.
+    int a; int b; int c; int cab; int vicky; is allowed.
+    int vicky is good; is not allowed.
+*/
+
+/* Note:
+    We must create meaningful variable names in our program. This enhances readability of our program.
+
+    These variables are stored in our RAM(Random Access Memory)
+*/
