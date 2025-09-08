d7f67e84bb221e67b64b9729aac666b25ea48bb8
VickyRohilla862 <145284256+VickyRohilla862@users.noreply.github.com>
2025-09-08 16:37:35 +0000

    modified:   Chapter_3/07_Switch_Case_Instructions.c 	new file:   Chapter_3_Practice_Set/Questions.c

--- Chapter_3/07_Switch_Case_Instructions.c
+++ Chapter_3/07_Switch_Case_Instructions.c
@@ -13,3 +13,72 @@ syntax:
     }
 */
 // The value of integer expression is matches against c1, c2, c3. If it matches any of these cases, that case along with all subsequenced "case" and "default" sattements are executed.
+
+//example code:
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    switch (a){
+        case 1:
+            printf("You entered 1");
+        case 2:
+            printf("You entered 2");
+        case 3:
+            printf("You entered 3");
+        case 4:
+            printf("You entered 4");
+        default:
+            printf("You didn't entered a value between 1-4. You entered %d", a);
+    }
+    return 0;
+}
+
+/*
+input: 
+    3
+*/
+/*
+output:
+    You entered 3
+    you entered 4
+    You didn't entered a value between 1-4. You entered 3
+*/
+
+// To avoid this output, you use break statement like this:
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    switch (a){
+        case 1:
+            printf("You entered 1");
+            break;
+        case 2:
+            printf("You entered 2");
+            break;
+        case 3:
+            printf("You entered 3");
+            break;
+        case 4:
+            printf("You entered 4");
+            break;
+        default:
+            printf("You didn't entered a value between 1-4. You entered %d", a);
+            break;
+    }
+    return 0;
+}
+
+// input: 3
+// output: You entered 3
+// Nothing else will be printed
+
+/*
+Note:
+    1. We can use switch case statements even by writing cases in any order of our choice (Not necessarily ascending).
+    2. Char values are allowed as they can be easily evaluated to an integer
+    3. A switch can occur within another but in practice this is rarely done.
+*/
---
+++ Chapter_3_Practice_Set/Questions.c
@@ -0,0 +1,24 @@
+/*
+1. What will be the output of the program:
+    int a = 10;
+    if(a = 11)
+        printf("I am 11");
+    else
+        printf("I am not 18");
+
+2. Write program to determine whether a student has passed or failed. To pass, a student requires a total of 40% and atleast 33% in each subject. Assume there are 3 subjects and take the marks as input from the user.
+
+3. Calculate the income tax paid by an employee to the government as per the slabs mentioned below:
+    Income Slab         Tab
+    --------------------------
+    2.5L-5.0L           5%
+    5.0L-10.0L          20%
+    Above 10L           30%
+Note that there is no tax below 2.5L. Take income amount as input from the user.
+
+4. Write a program to find whether a year entered by the user is a leap year or not. Take year as input from the user.
+
+5. Write a program to determine whether a charactered entered by the user is lowercase or not.
+
+6. Write a program to find greatest of 4 numbers entered by the user.
+*/
