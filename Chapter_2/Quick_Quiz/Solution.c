b912aee4c6219c1cdd7a90d2f39550c6831a6e89
VickyRohilla862 <145284256+VickyRohilla862@users.noreply.github.com>
2025-09-08 15:55:54 +0000

    renamed:    Chapter_2/04_Quick_Quiz.c -> Chapter_2/Quick_Quiz/Quick_Quiz.c 	new file:   Chapter_2/Quick_Quiz/Solution.c

---
+++ Chapter_2/Quick_Quiz/Solution.c
@@ -0,0 +1,3 @@
+// As we all know that the arithmatic operation between a float and an integer always results in a float
+// so when we solve the question : int k = 3.0/9, the answer will be 0.33333 or something like that. But the variable "k" is of int type so it will not store the float value
+// So the exact value of "k" will be 0.
