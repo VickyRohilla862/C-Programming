e56c9e4ef2a7cbbfe96218a14bc509538008af61
VickyRohilla862 <145284256+VickyRohilla862@users.noreply.github.com>
2025-09-08 16:02:27 +0000

    new file:   Chapter_3/07_Switch_Case_Instructions.c

---
+++ Chapter_3/07_Switch_Case_Instructions.c
@@ -0,0 +1,15 @@
+// Switch case is used when we have to make a choice between numbers of alternatives for a given variable
+/*
+syntax:
+    switch (integer_expression){
+        case c1:
+            //code
+        case c2:
+            //code
+        case c3:
+            //code
+        default:
+            //code
+    }
+*/
+// The value of integer expression is matches against c1, c2, c3. If it matches any of these cases, that case along with all subsequenced "case" and "default" sattements are executed.
