dc8655d49e3c03db0fab66dc4ddf6dab2bd74c36
Vicky Rohilla <145284256+VickyRohilla862@users.noreply.github.com>
2025-09-10 11:49:59 +0000

    deleted:    .devcontainer/Dockerfile 	deleted:    .devcontainer/devcontainer.json 	deleted:    .devcontainer/reinstall-cmake.sh 	deleted:    .github/dependabot.yml 	deleted:    .vscode/tasks.json 	new file:   Chapter_4/01_Loop_Control_Instructions.c

---
+++ Chapter_4/01_Loop_Control_Instructions.c
@@ -0,0 +1,30 @@
+/*
+Sometimes we want a part of code to run multiple times but the number of time we want to run that code is higher than we can do it manually.
+In that case loops comes in handy.
+For example:
+    We want to print "Happy Birthday" 10 times. You can do it by using printf function 10 times like this:
+*/
+
+// #include <stdio.h>
+// int main(){
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     printf("Happy Birthday!");
+//     return 0;
+// }
+
+/*
+But what if I ask you to print it 100 times or 200 times?..
+You'll probably say "Are you serious?" But i am serious.. what will you do?
+Here you can use loops.
+Hence, loops makes it easy for a programtp tell the computer that a given set of instructions must be executed repeatedly.
+*/
+
+// Check out the types of loops in the next File..
